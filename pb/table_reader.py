b197a8b5af1daf5fb5e8ab9ab81b8c6dc73713c2
Francis Wang <go717franciswang@hotmail.com>
2014-05-26 18:49:53 -0700

    Locating balls successfully

--- pb/table_reader.py
+++ pb/table_reader.py
@@ -1,74 +1,95 @@
 import cv2
 import numpy as np
+from matplotlib import pyplot as plt
 
 class TableReader:
     def __init__(self, img):
-        """docstring for __init__"""
-        pass
+        self.original = img.copy()
+        self.hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+        self.gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    def get_dominant_color_range(self):
+        bounds = [[], []]
+        for i in xrange(3):
+            hist, bins = np.histogram(self.hsv[:,:,i], 100)
+            samples = float(np.sum(hist))
+            maxarg = np.argmax(hist)
+            min_bound = maxarg
+            max_bound = maxarg
+
+            while min_bound > 0:
+                min_bound -= 1
+                if hist[min_bound] / samples < 0.005:
+                    min_bound += 1
+                    break
+
+            while max_bound < 99:
+                if hist[max_bound] / samples < 0.005:
+                    max_bound -= 1
+                    break
+                max_bound += 1
+
+            bounds[0].append(np.int(bins[min_bound]))
+            bounds[1].append(np.int(bins[max_bound+1]))
+        return map(tuple,bounds)
+
+    def get_masked_background(self):
+        l,u = self.get_dominant_color_range()
+        masked = cv2.inRange(self.hsv, l, u)
+        # plt.imshow(masked)
+        # plt.show()
+        return masked
+
+    def get_masked_black(self):
+        masked = cv2.inRange(self.hsv, (0,0,50), (360,360,100))
+        # plt.imshow(masked)
+        # plt.show()
+        return masked
+
+    def get_masked(self):
+        masked = cv2.bitwise_or(self.get_masked_background(), self.get_masked_black())
+        # plt.imshow(masked)
+        # plt.show()
+        return masked
+
+    def get_gray_img_no_background(self):
+        masked = self.get_masked_black()
+        # masked = cv2.medianBlur(masked,3)
+        # masked = cv2.blur(masked,(3,3))
+        masked_inv = cv2.bitwise_not(masked)
+
+        img2 = cv2.bitwise_not(self.gray, mask=masked_inv)
+        # plt.imshow(img2)
+        # plt.show()
+        return self._get_circles(img2)
+
+    def _get_circles(self, img):
+        # img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        # cv2.blur(img, (4,4))
+        # img = cv2.medianBlur(img,3)
+        circles = cv2.HoughCircles(img,cv2.cv.CV_HOUGH_GRADIENT,1,5, 
+                param1=100,param2=10,minRadius=6,maxRadius=10)
+        # print circles
+
+        circles = np.uint16(np.around(circles))
+        for i in circles[0,:]:
+            # draw the outer circle
+            cv2.circle(self.original,(i[0],i[1]),i[2],(0,255,0),2)
+            # draw the center of the circle
+            cv2.circle(self.original,(i[0],i[1]),2,(0,0,255),3)
+
+        self.original = cv2.cvtColor(self.original, cv2.COLOR_BGR2RGB)
+        plt.imshow(self.original)
+        plt.show()
+        return circles
 
     def get_table(self):
         """docstring for get_table"""
         pass
 
 if __name__ == '__main__':
-    # convert image to HSV
-    # use masking to get rid of background color
-    # make everything else the same color
-    # use Hough circle transform to detect the balls
-    '''hough circle transform'''
-    '''
-    img = cv2.imread('resources/table1.jpg',0)
-    cimg = cv2.imread('resources/table1.jpg',1)
-    img = cv2.medianBlur(img,5)
-
-    circles = cv2.HoughCircles(img,cv2.cv.CV_HOUGH_GRADIENT,1,20,
-                                param1=50,param2=30,minRadius=0,maxRadius=0)
-    print circles
-
-    circles = np.uint16(np.around(circles))
-    for i in circles[0,:]:
-        # draw the outer circle
-        cv2.circle(cimg,(i[0],i[1]),i[2],(0,255,0),2)
-        # draw the center of the circle
-        cv2.circle(cimg,(i[0],i[1]),2,(0,0,255),3)
-
-    cv2.imshow('detected circles',cimg)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-    '''
-    
-    '''watershed
-    http://opencv-python-tutroals.readthedocs.org/en/latest/py_tutorials/py_imgproc/py_watershed/py_watershed.html'''
-    from matplotlib import pyplot as plt
-
-    img = cv2.imread('resources/table1.jpg')
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    ret, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
-    plt.imshow(thresh)
-    plt.show()
-
-    # noise removal
-    kernel = np.ones((3,3),np.uint8)
-    opening = cv2.morphologyEx(thresh,cv2.MORPH_OPEN,kernel, iterations = 2)
-
-    # sure background area
-    sure_bg = cv2.dilate(opening,kernel,iterations=3)
-
-    # Finding sure foreground area
-    dist_transform = cv2.distanceTransform(opening,cv2.cv.CV_DIST_L2,5)
-    ret, sure_fg = cv2.threshold(dist_transform,0.7*dist_transform.max(),255,0)
-
-    # Finding unknown region
-    sure_fg = np.uint8(sure_fg)
-    unknown = cv2.subtract(sure_bg,sure_fg)
-
-    # Marker labelling
-    ret, markers = cv2.connectedComponents(sure_fg)
-
-    # Add one to all labels so that sure background is not 0, but 1
-    markers = markers+1
-
-    # Now, mark the region of unknown with zero
-    markers[unknown==255] = 0
-
+    for i in xrange(1,5):
+        img = cv2.imread('resources/table%d.jpg' % (i,))
+        table = TableReader(img)
+        table.get_gray_img_no_background()
 
