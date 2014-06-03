4c8a6f39fb106d0ab77a7e4b45a93a1c69615329
Francis Wang <go717franciswang@hotmail.com>
2014-06-02 23:44:07 -0700

    Implement table_viewer

--- pb/table_viewer.py
+++ pb/table_viewer.py
@@ -1,10 +1,25 @@
+import cv2
 from matplotlib import pyplot as plt
 import numpy as np
+import ball as bll
 
 class TableViewer:
     def display(self, table, block=False):
         img = np.zeros(shape=(table.w, table.h, 3), dtype='uint8')
         img[:,:,1] = np.ones(shape=(table.w, table.h), dtype='uint8') * 255
+
+        for ball in table.get_balls():
+            pos = ball.get_pos()
+            r = ball.r
+            if ball.type == bll.TYPE_CUE:
+                cv2.circle(img, pos, r, (255,255,255))
+            elif ball.type == bll.TYPE_BLACK:
+                cv2.circle(img, pos, r, (0,0,0))
+            elif ball.type == bll.TYPE_STRIPE:
+                cv2.circle(img, pos, r, (255,0,0))
+            elif ball.type == bll.TYPE_SOLID:
+                cv2.circle(img, pos, r, (0,0,255))
+
         plt.imshow(img)
         plt.draw()
         plt.show(block=block)
@@ -12,6 +27,10 @@ class TableViewer:
 if __name__ == '__main__':
     import table
     t = table.Table(200, 300)
+    t.add_ball(bll.Ball(150,100,9,bll.TYPE_CUE))
+    t.add_ball(bll.Ball(250,100,9,bll.TYPE_BLACK))
+    t.add_ball(bll.Ball(250,50,9,bll.TYPE_STRIPE))
+    t.add_ball(bll.Ball(250,150,9,bll.TYPE_SOLID))
     table_viewer = TableViewer()
     table_viewer.display(t, True)
 
